5805a1b7c78a285e2347b9c343de265c7cba3b0b
Kyle Swanson <k@ylo.ph>
2015-05-23 21:31:54 -0500

    Create fades.c

---
+++ fades.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+#define NUM_SAMPLES 48000 
+
+int main(void) {
+    for(int i = 0; i <= NUM_SAMPLES; i++) {
+        //FADE-IN
+        float linear_in = i / NUM_SAMPLES;
+        float half_sine_in = sin(M_PI / 2 * i / NUM_SAMPLES);
+        float log_fade_in = log(1 + (((float) i / NUM_SAMPLES) * (M_E - 1)));
+
+        //FADE-OUT
+        float linear_out = 1 - linear_in;
+        float half_sine_out = 1 - half_sine_in;
+        float log_fade_out = 1 - log_fade_in;
+    }
+}
