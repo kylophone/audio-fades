d310ebf1a8e56e124f187c965a61cb6cd2ca901e
Kyle Swanson <kylophone@gmail.com>
2015-05-26 10:13:54 -0500

    added example

---
+++ example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "fades.h"
+
+#define FADE_LENGTH 48000
+
+int main(void) {
+    for(int i = 0; i < FADE_LENGTH; i++) {
+        printf("%.10f\n", fade_envelope(LOG_IN, FADE_LENGTH - 1, i));
+    }
+    return 0;
+}
---
+++ fades.h
@@ -0,0 +1,28 @@
+#include <math.h>
+
+
+#define LINEAR_IN 0
+#define HALF_SINE_IN 1
+#define LOG_IN 2
+#define LINEAR_OUT 3
+#define HALF_SINE_OUT 4
+#define LOG_OUT 5
+
+float fade_envelope(int this_fade, int fade_length, int sample_number) {
+    switch (this_fade) {
+        case LINEAR_IN:
+            return (float) sample_number / fade_length;
+        case HALF_SINE_IN:
+            return sin(M_PI / 2 * sample_number / fade_length);
+        case LOG_IN:
+            return log(1 + (((float) sample_number / fade_length) * (M_E - 1)));
+        case LINEAR_OUT:
+            return 1.0 - ((float) sample_number / fade_length);
+        case HALF_SINE_OUT:
+            return 1.0 - (sin(M_PI / 2 * sample_number / fade_length));
+        case LOG_OUT:
+            return 1.0 - (log(1 + (((float) sample_number / fade_length) * (M_E - 1))));
+        default:
+            return 1.0;
+    }
+}
